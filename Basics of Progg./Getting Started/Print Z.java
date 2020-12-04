9b6a98f54d90e529f21aa40d66ed8fc5e976147c
Joyal S Palackel <joyal.palackel19@gmail.com>
2020-12-04 17:20:43 +0530

    Create Print Z.java

---
+++ Basics of Progg./Getting Started/Print Z.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("*****");
+        System.out.print("   * ");
+        System.out.print("  *  ");
+        System.out.print(" *   ");
+        System.out.print("*****");
+        
+    
+
+    }
+}
