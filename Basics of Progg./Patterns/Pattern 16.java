86f133df49d685223447e514afaafae29213688a
Joyal S Palackel <joyal.palackel19@gmail.com>
2020-12-04 22:03:36 +0530

    Create Pattern 16.java

---
+++ Basics of Progg./Patterns/Pattern 16.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+     int n =scn.nextInt();
+     
+     int sp = (n*2)-3;
+     int st = 1;
+     int val =1;
+     for(int i=1; i<=n; i++)
+     {
+         for(int j=1; j<=st; j++)
+         {
+             System.out.print(val+"\t");
+             val++;
+         }
+         
+         for(int k=1; k<=sp; k++)
+         {
+             System.out.print("\t");
+         }
+        
+        
+         if (i==n)
+         {
+             st--;
+             val--;
+         }
+         for(int l=1; l<=st; l++)
+         {
+             val--;
+             System.out.print(val+"\t");
+         }
+         System.out.println();
+         
+         
+         st++; 
+         sp = sp-2;
+     }
+
+ }
+}
