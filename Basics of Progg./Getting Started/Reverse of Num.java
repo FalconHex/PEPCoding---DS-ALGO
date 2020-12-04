54ee394ff30a03795aba090dbdb9ec981b0e94b3
Joyal S Palackel <joyal.palackel19@gmail.com>
2020-12-04 20:33:48 +0530

    Create Reverse of Num.java

---
+++ Basics of Progg./Getting Started/Reverse of Num.java
@@ -0,0 +1,14 @@
+import java.util.*;
+   
+   public class Main{
+   
+   public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int num = sc.nextInt();
+     while(num>0)
+     {
+         System.out.println(num%10);
+         num=num/10;
+     }
+    }
+   }
